77a840de5ee9815d6d3566ddd1dbd3eecce47f57
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2026-03-30 11:42:00 +0300

    feat: implement OutputWriter service

---
+++ src/services/output/OutputWriter.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import ConfigService from "../../config/ConfigService"; import DomainResult from "../../interfaces/DomainResult";
+import IService from "../../interfaces/IService";
+import LoggerService from "../../logger/LoggerService";
+
+class OutputWriter implements IService {
+    private hasWritten: boolean = false
+
+    constructor(
+        private config: ConfigService,
+        private logger: LoggerService
+    ) { }
+
+    async initialize(): Promise<void> { }
+
+    async cleanup(): Promise<void> {
+        await fs.promises.appendFile(this.config.getOutputFile(), '\n]')
+        this.logger.debug(`Output finalized: ${this.config.getOutputFile()}`)
+    }
+
+    async write(result: DomainResult): Promise<void> {
+        const json = JSON.stringify(result, null, 2)
+
+        if (!this.hasWritten) {
+            await fs.promises.writeFile(this.config.getOutputFile(), '[\n' + json)
+            this.hasWritten = true
+        } else {
+            await fs.promises.appendFile(this.config.getOutputFile(), ',\n' + json)
+        }
+
+        this.logger.debug(`Written result for ${result.domain}`)
+    }
+}
+
+export default OutputWriter
